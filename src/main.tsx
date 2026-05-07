fd94408a39feb0a8586268655a3d8ad282f7f9f8
michael-m-m365group <micheal.m@m365connect.com>
2026-05-07 20:31:01 +0300

    error trouble shoots

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
+import { Toaster } from 'sonner'
 import App from './App'
 import './index.css'
 
@@ -10,10 +10,8 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BlinkUIProvider theme="linear" darkMode="light">
-        <Toaster />
-        <App />
-      </BlinkUIProvider>
+      <App />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>,
 )
